2c5bdacda59cc8210f22b12882a9d36588f9f5a1
AsdZy <dmitry.ildyukov@yandex.ru>
2023-05-27 20:06:04 +0300

    Начал делать 6 лабу

---
+++ lw6/Student/Student/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CStudent student1 = new CStudent("Name_1");
+            student1.Age = 9;
+            {
+                CStudent student2 = new CStudent("Name_2");
+                CStudent student3 = new CStudent("Name_3");
+            }
+            CStudent student4 = new CStudent("Name_4");
+            CStudent Student5 = new CStudent("Name_5");
+        }
+    }
+}
---
+++ lw6/Student/Student/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class CStudent
+    {
+        public CStudent(string name)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(name))
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error message: {e.Message}\nError type: {e.GetType()}");
+            }
+        }
+
+
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        public string Patronymic
+        {
+            get { return patronymic; }
+            set { patronymic = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        private string name;
+        private string surname;
+        private string patronymic;
+        private int age;
+    }
+}
